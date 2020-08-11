e42673c8a4bf83a4ccc0bf75a495a28c39b3086b
TechQuery <shiy007@qq.com>
2020-08-11 14:43:05 +0800

    [fix] PWA updating [optimize] update Upstream packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import { auto } from 'browser-unhandled-rejection';
+import { serviceWorkerUpdate } from 'web-utility';
 import { documentReady, render, createCell } from 'web-cell';
 
 import { PageRouter } from './page';
-
-if ('serviceWorker' in navigator) navigator.serviceWorker.register('./sw.ts');
 
 auto();
 
@@ -16,5 +15,19 @@ self.addEventListener('unhandledrejection', event => {
 
     self.alert(message);
 });
+
+const { serviceWorker } = window.navigator;
+
+serviceWorker
+    ?.register('/sw.js')
+    .then(serviceWorkerUpdate)
+    .then(worker => {
+        if (window.confirm('New version of this Web App detected, update now?'))
+            worker.postMessage({ type: 'SKIP_WAITING' });
+    });
+
+serviceWorker?.addEventListener('controllerchange', () =>
+    window.location.reload()
+);
 
 documentReady.then(() => render(<PageRouter />));
